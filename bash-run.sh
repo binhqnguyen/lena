f0eadce7deb91d9c9c7d06498765f4a60f4b70b1
Binh Q Nguyen <binhn@tolkien.eng.utah.edu>
2013-12-18 00:35:33 -0700

    Update running scripts x2--

--- bash-run.sh
+++ bash-run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 ##a2a4 with ctr-error=0
-echo "running a2a4 rlcam seamless no ctr-error..."
-./x2-auto-ho-a2a4 a2a4_rlcam_seamless_no_ctrerror 200 300 0
+echo "running a2a4 rlcam lossless no ctr-error..."
+./x2-auto-ho-a2a4 a2a4_rlcam_lossless_no_ctrerror 200 300 0 radio_dev
 
 ##a3 with ctr-error=0
-echo "running a3 rlcam seamless ctr-error..."
-./x2-auto-ho-a3 a3_rlcam_seamless_no_ctrerror 200 300 0
+echo "running a3 rlcam lossless ctr-error..."
+./x2-auto-ho-a3 a3_rlcam_lossless_no_ctrerror 200 300 0 radio_dev
 
 
-perl -pi -e 's/EpsBearerToRlcMapping \"RlcAmAlways\"/EpsBearerToRlcMapping \"RlcAmAlways\"/g' lte.in
+perl -pi -e 's/EpsBearerToRlcMapping \"RlcAmAlways\"/EpsBearerToRlcMapping \"RlcUmAlways\"/g' lte.in
 
 ##a2a4 without ctr-error=0
 echo "running a2a4 rlcum lossless ctr-error..."
-./x2-auto-ho-a2a4 a2a4_rlcum_lossless_no_ctrerror 200 300 0
+./x2-auto-ho-a2a4 a2a4_rlcum_lossless_no_ctrerror 200 300 0 radio_dev
 
 ##a3 with ctr-error=0
 echo "running a3 rlcum lossless  ctr-error..."
-./x2-auto-ho-a3 a3_rlcum_lossless_no_ctrerror 200 300 0
+./x2-auto-ho-a3 a3_rlcum_lossless_no_ctrerror 200 300 0 radio_dev
 
