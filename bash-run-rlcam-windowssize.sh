5b4821076eb53d8510179d1ac1d837b3be81611f
Binh Q Nguyen <binhn@tolkien.eng.utah.edu>
2014-01-09 09:27:43 -0700

    Fix scripts to work with lena version, for lena_dev, changes needed.

--- bash-run-rlcam-windowssize.sh
+++ bash-run-rlcam-windowssize.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-
-poll=2
+radio="radio"
+if [ $# -lt 1 ]; then
+	echo "Usage: <this-script> <Number of radio RBs>"
+	exit
+fi
+poll=1
 last_poll=$poll
-wsize=16
+wsize=8
 last_wsize=$wsize
 ##clean the log file
-echo "" >& BASH_LOG 
-while [ $wsize -le 2048 ]; do
+touch BASH_LOG
+perl -pi -e "s/m_windowSize = 512;/m_windowSize = $wsize;/g" src/lte/model/lte-rlc-am.cc
+while [ $wsize -le 1024 ]; do
 				perl -pi -e "s/m_windowSize = $last_wsize;/m_windowSize = $wsize;/g" src/lte/model/lte-rlc-am.cc
-				let poll=2
+				let poll=1
 				let last_poll=$poll
 				###poll has to be smaller than window size.
 				while [ $poll -le $wsize ]; do
 								perl -pi -e "s/m_pollPdu = $last_poll;/m_pollPdu = $poll;/g" src/lte/model/lte-rlc-am.cc
-								./lte-nsc rlcam-windowsize-$wsize-poll-$poll-largePollByte 15 400 -1 radio
+								./lte-nsc rlcam-windowsize-$wsize-poll-$poll-$1-RBs 15 400 -1 $radio
 								echo "(poll, wsize) = $poll $wsize" >> BASH_LOG
 								let last_poll=poll
 								let poll=poll*2
--- bash-run.sh
+++ bash-run.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 ##a2a4 with ctr-error=0
-echo "running a2a4 rlcam lossless no ctr-error..."
-./x2-auto-ho-a2a4 a2a4_rlcam_lossless_no_ctrerror 200 300 0 radio_dev
+echo "running a2a4 rlcam newlossless no ctr-error..."
+./x2-auto-ho-a2a4 a2a4_rlcam_newlossless_no_ctrerror 200 300 0 radio
 
 ##a3 with ctr-error=0
-echo "running a3 rlcam lossless ctr-error..."
-./x2-auto-ho-a3 a3_rlcam_lossless_no_ctrerror 200 300 0 radio_dev
+echo "running a3 rlcam newlossless ctr-error..."
+./x2-auto-ho-a3 a3_rlcam_newlossless_no_ctrerror 200 300 0 radio
 
 
 perl -pi -e 's/EpsBearerToRlcMapping \"RlcAmAlways\"/EpsBearerToRlcMapping \"RlcUmAlways\"/g' lte.in
 
 ##a2a4 without ctr-error=0
-echo "running a2a4 rlcum lossless ctr-error..."
-./x2-auto-ho-a2a4 a2a4_rlcum_lossless_no_ctrerror 200 300 0 radio_dev
+echo "running a2a4 rlcum newlossless ctr-error..."
+./x2-auto-ho-a2a4 a2a4_rlcum_newlossless_no_ctrerror 200 300 0 radio
 
 ##a3 with ctr-error=0
-echo "running a3 rlcum lossless  ctr-error..."
-./x2-auto-ho-a3 a3_rlcum_lossless_no_ctrerror 200 300 0 radio_dev
+echo "running a3 rlcum newlossless  ctr-error..."
+./x2-auto-ho-a3 a3_rlcum_newlossless_no_ctrerror 200 300 0 radio
 
 perl -pi -e 's/EpsBearerToRlcMapping \"RlcUmAlways\"/EpsBearerToRlcMapping \"RlcAmAlways\"/g' lte.in
-./bash-run-rlcam-windowssize.sh 
+./bash-run-rlcam-windowssize.sh 25
 
 
-perl -pi -e 's/EpsBearerToRlcMapping \"RlcUmAlways\"/EpsBearerToRlcMapping \"RlcUmAlways\"/g' lte.in
